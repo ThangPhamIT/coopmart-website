4e5ce076e910bea29b19b6815a4f41257927c3b1
ThangPham <pnthang@tma.com.vn>
2018-11-13 21:12:07 +0700

    update build folder

--- build/precache-manifest.b8e991255cfddf04b9877a7f7a1c6db9.js
+++ build/precache-manifest.b8e991255cfddf04b9877a7f7a1c6db9.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/static/media/outline-icons.cd6c777f.woff2"
   },
   {
-    "revision": "6a85a4978ebb84654f2e",
-    "url": "/static/css/main.6dfcd0d7.chunk.css"
+    "revision": "8b00aa66a90dac9b3f00",
+    "url": "/static/css/main.a4b76746.chunk.css"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
@@ -56,40 +56,40 @@ self.__precacheManifest = [
     "url": "/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/static/media/outline-icons.701ae6ab.eot"
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
   },
   {
-    "revision": "6a85a4978ebb84654f2e",
-    "url": "/static/js/main.6a85a497.chunk.js"
+    "revision": "8b00aa66a90dac9b3f00",
+    "url": "/static/js/main.8b00aa66.chunk.js"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/static/media/outline-icons.ef60a4f6.woff"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/static/media/icons.962a1bf3.svg"
-  },
-  {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
     "url": "/static/media/outline-icons.ad97afd3.ttf"
-  },
-  {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/static/media/brand-icons.13db00b7.eot"
   },
   {
-    "revision": "e8c322de9658cbeb8a774b6624167c2c",
-    "url": "/static/media/brand-icons.e8c322de.woff2"
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
     "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
@@ -120,7 +120,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.447edd45.chunk.css"
   },
   {
-    "revision": "eca770b8ace7731271824fe343afd9ec",
+    "revision": "cf2efb40ba2d1fcd6b2214e3c84c135d",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d33c69e1ea75a888898d990da60596a.js"
+  "/precache-manifest.b8e991255cfddf04b9877a7f7a1c6db9.js"
 );
 
 workbox.clientsClaim();
